79253c6efaa847a51562c022d851f33c6c5b10bf
Amul <amulbrar24@gmail.com>
2024-07-14 00:44:16 +0530

    add resolution and random rgb values

---
+++ script.js
@@ -0,0 +1,48 @@
+let promptBtn = document.querySelector('.prompt-btn');
+
+promptBtn.addEventListener('click', () => {
+    let resolution = 0;
+    while (resolution <= 0 || resolution > 100) {
+        resolution = prompt("Enter number of squares per side (1- 100): ", "32");
+    }
+    createGrid(resolution)
+});
+
+function rgbValues() {
+    let redValue = Math.random() * 255;
+    let greenValue = Math.random() * 255;
+    let blueValue = Math.random() * 255;
+
+    return `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+}
+
+let containerDiv = document.querySelector('.container');
+
+let randomBtn = document.querySelector('.random-btn');
+let isRandomColors = false;
+randomBtn.addEventListener('click', () => {
+    isRandomColors = !isRandomColors;
+})
+
+function createGrid(resolution) {
+    containerDiv.innerHTML = '';
+    const squareSize = 640 / resolution;
+
+    for (let i = 0; i < (resolution * resolution); i++) {
+        const square = document.createElement("div");
+        square.classList.add('square');
+
+        square.style.width = `${squareSize}px`
+        square.style.height = `${squareSize}px`
+
+        square.addEventListener('mouseenter', () => {
+            if (isRandomColors) {
+                square.style.backgroundColor = rgbValues();
+            } else {
+                square.style.backgroundColor = "red";
+            }
+        });
+
+        containerDiv.appendChild(square);
+    }
+}
